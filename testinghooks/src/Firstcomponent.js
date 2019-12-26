94e901cfb277f62c7ccbb23b57e42ef2506cb6bb
Martin Crabtree <martin.crabtree@gmail.com>
2019-12-26 15:22:44 -0500

    got useEffect to update doc title

--- testinghooks/src/Firstcomponent.js
+++ testinghooks/src/Firstcomponent.js
@@ -1,16 +1,22 @@
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 export default(props)=>{
 
+    // initialize state with count set to 0, also get update function
     const [count, setCount] = useState(0);
+
+    // similar to component did update/mount
+    useEffect(() => {
+        document.title=`The counter is currently ${count}`
+    });
     
     return(
         <div>
         <p>Current count is { count }</p>
         <div>
-            <button >increase count</button>
+            <button onClick={()=>setCount(count+1)} >increase count</button>
         </div>
 
         </div>
